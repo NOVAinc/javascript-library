26b6c63b5aad82033cb93933e882328d19a375b2
nigra <19545604+NOVAinc@users.noreply.github.com>
2025-03-18 00:12:54 +0100

    Port book constructor from object function to class

--- script.js
+++ script.js
@@ -15,11 +15,13 @@ const myLibrary = [
   },
 ];
 
-function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
+class Book {
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
 }
 
 function addBookToLibrary(title, author, pages, read) {
